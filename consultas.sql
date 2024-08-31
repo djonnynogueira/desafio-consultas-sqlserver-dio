5928446eef78d7f725e6de50a679bf781255a903
Djonny Nogueira <djonnynogueira@gmail.com>
2024-08-31 15:20:23 -0300

    Consultas SQL desadio DIO - Trilha .NET - Banco de Dados

---
+++ consultas.sql
@@ -0,0 +1,9 @@
+/* 
+===============================================================================
+Autor:           Djonny Nogueira
+Data:            31--8-2024
+Descrição:       Consultas SQL desadio DIO - Trilha .NET - Banco de Dados                 
+Tabela(s) Afetada(s):  Filmes, Atores, Generos, ElencoFilme, FilmesGenero
+===============================================================================
+*/
+
